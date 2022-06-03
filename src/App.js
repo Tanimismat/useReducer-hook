7416313244dd9f1e0e4dd489dcbb2e7886718c14
Tanimismat <tanim.ismat@gmail.com>
2022-06-03 23:54:50 +0600

    parsing error

--- src/App.js
+++ src/App.js
@@ -1,19 +1,45 @@
-import { useState } from 'react';
+import { useReducer, useState } from 'react';
+
+
+function reducer(state, action) {
+  // return {count: state.count + 1}
+  switch (action.type) {
+    case "increment":
+      // return (count: state.count+1);
+      // return {
+      //   ...state,
+      //   [action.name]: state[action.name] + 1,
+      // };
+    case "decrement":
+      // return {
+      //   ...state,
+      //   [action.name]: state[action.name] - 1,
+      // };
+    default:
+      return state;
+  }
+}
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [state, dispatch] = useReducer(reducer, {count:0})
+
+  // const [count, setCount] = useState(0)
   
   function increment() {
-    setCount(previousCount => previousCount + 1)
+    // setCount(previousCount => previousCount + 1)
+    // dispatch()
+    dispatch({type:'increment'})
   }
   
   function decrement() {
-    setCount(previousCount => previousCount-1)
+    // setCount(previousCount => previousCount-1)
+    dispatch({type:'decrement'})
   }
   return (
     <>
       <button onClick={increment}>+</button>
-      <span> {count} </span>
+      {/* <span> {count} </span> */}
+      <span> {state.count} </span>
       <button onClick={decrement}>-</button>
     </>
   );
